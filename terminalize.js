5bf51bce2e45b1d05601f6aa877f0decb808ab69
kjwon15 <kjwonmail@gmail.com>
2014-08-16 14:44:32 +0900

    handle return key, do command

--- terminalize.js
+++ terminalize.js
@@ -7,6 +7,9 @@ var terminalize = function(elem) {
   var input = $('<input>');
   var lis = $(elem).children('li');
 
+  var ps1Str = '<span class="terminal-red">|ID|</span>@<span class="terminal-blue">|HOST|</span>:<span class="terminal-yellow">|PWD|</span>$&nbsp;';
+  var pwd = '/';
+
   var print = function(msg, isHtml) {
     var pre = $('<pre>');
     pre.addClass('terminal-output');
@@ -18,6 +21,31 @@ var terminalize = function(elem) {
     log.append(pre);
   };
 
+  var keyHandler = function(event) {
+    switch(event.keyCode) {
+      case 0x0d: // Return
+        event.preventDefault();
+        doCommand();
+        parsePs1();
+        break;
+    }
+  };
+
+  var doCommand = function() {
+    var command = input.val();
+    var psStr = ps1.html();
+    print(psStr + command, true);
+    input.val(null);
+  };
+
+  var parsePs1 = function() {
+    ps1.html(ps1Str
+        .replace('|ID|', 'guest')
+        .replace('|HOST|', 'localhost')
+        .replace('|PWD|', pwd)
+        );
+  }
+
 
   main.addClass('terminal-main');
   log.addClass('terminal-log');
@@ -26,12 +54,16 @@ var terminalize = function(elem) {
   input.addClass('terminal-input');
 
 
+  parsePs1();
+
   main.append(log);
   main.append(lastLine);
 
   lastLine.append(ps1);
   lastLine.append(inputWrap);
   inputWrap.append(input);
+
+  input.on('keydown', keyHandler);
 
   $(elem).after(main);
   $(elem).remove();
