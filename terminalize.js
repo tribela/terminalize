7b849430b477bd31f7cf6823509b79811485d0b7
kjwon15 <kjwonmail@gmail.com>
2014-08-16 14:22:46 +0900

    main frame

---
+++ terminalize.js
@@ -0,0 +1,28 @@
+var terminalize = function(elem) {
+  var main = $('<div>');
+  var log = $('<div>');
+  var lastLine = $('<div>');
+  var ps1 = $('<span>');
+  var inputWrap = $('<div>');
+  var input = $('<input>');
+  var lis = $(elem).children('li');
+
+
+  main.addClass('terminal-main');
+  log.addClass('terminal-log');
+  ps1.addClass('terminal-ps1');
+  inputWrap.addClass('terminal-input-wrap');
+  input.addClass('terminal-input');
+
+
+  main.append(log);
+  main.append(lastLine);
+
+  lastLine.append(ps1);
+  lastLine.append(inputWrap);
+  inputWrap.append(input);
+
+  $(elem).after(main);
+  $(elem).remove();
+  input.focus();
+};
